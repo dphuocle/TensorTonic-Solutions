5d3d0d2a739c6c5773c2eb418ba26080b9dffb8f
Doan Phuoc LE <109648895+dphuocle@users.noreply.github.com>
2026-03-23 18:06:03 +0100

    Add solution for metrics-f1-micro

---
+++ metrics-f1-micro/metrics-f1-micro.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def f1_micro(y_true, y_pred) -> float:
+    """
+    Compute micro-averaged F1 for multi-class integer labels.
+    """
+    # Write code here
+    y_true = np.asarray(y_true)
+    y_pred = np.asarray(y_pred)
+    tp = np.count_nonzero(y_true == y_pred)
+    fp = len(y_pred) - tp
+    fn = len(y_true) - tp
+    f1_micro = 2*tp / (2*tp + fp + fn)
+    return f1_micro
