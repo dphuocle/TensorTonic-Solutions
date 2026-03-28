8a0b478295a5c4b22268609b08ec47243956292f
Doan Phuoc LE <109648895+dphuocle@users.noreply.github.com>
2026-03-28 16:56:56 +0100

    Update solution for positional-encoding

--- positional-encoding/positional-encoding.py
+++ positional-encoding/positional-encoding.py
@@ -6,15 +6,13 @@ def positional_encoding(seq_len, d_model, base=10000.0):
     Odd d_model -> last column is sin.
     """
     # Write code here
-    pos = np.arange(seq_len)[:, None]              # shape: (seq_len, 1)
+    pos = np.arange(seq_len)[:, None]
     num_freqs = (d_model + 1) // 2
-    i = np.arange(num_freqs)[None, :]              # shape: (1, num_freqs)
+    i = np.arange(num_freqs)[None, :]
 
-    denom = base ** (2 * i / d_model)              # shape: (1, num_freqs)
-    angle = pos / denom                            # shape: (seq_len, num_freqs)
+    angle = pos / (base ** (2*i/d_model))
 
     pe = np.zeros((seq_len, d_model), dtype=float)
-    pe[:, 0::2] = np.sin(angle)                    # các cột chẵn
-    pe[:, 1::2] = np.cos(angle[:, :d_model // 2])  # các cột lẻ
-
+    pe[:, 0::2] = np.sin(angle)
+    pe[:, 1::2] = np.cos(angle[:, :d_model//2])
     return pe
